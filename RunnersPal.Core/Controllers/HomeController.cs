1941542add560050da1d4ba44cb4b0c2920aa1de
Andy Whitfield <andy@andywhitfield.me.uk>
2023-12-15 11:52:03 +0000

    Upgraded to net8

--- RunnersPal.Core/Controllers/HomeController.cs
+++ RunnersPal.Core/Controllers/HomeController.cs
@@ -11,81 +11,76 @@ using RunnersPal.Core.Data;
 using RunnersPal.Core.Extensions;
 using RunnersPal.Core.Models;
 
-namespace RunnersPal.Core.Controllers
+namespace RunnersPal.Core.Controllers;
+
+public class HomeController(ILogger<HomeController> logger) : Controller
 {
-    public class HomeController : Controller
+    public ActionResult Index() => View();
+
+    public ActionResult Error() => View("Error");
+
+    [HttpPost]
+    public IActionResult Login()
     {
-        private readonly ILogger<HomeController> logger;
+        var returnPage = Request.Form["return_page"].FirstOrDefault();
+        if (string.IsNullOrWhiteSpace(returnPage)) returnPage = Url.Content("~/");
+        Uri returnPageUri;
+        Uri.TryCreate(returnPage, UriKind.RelativeOrAbsolute, out returnPageUri);
+        if (returnPageUri == null || returnPageUri.IsAbsoluteUri)
+            returnPage = Url.Content("~/");
+        HttpContext.Session.SetString("login_returnPage", returnPage);
 
-        public HomeController(ILogger<HomeController> logger) => this.logger = logger;
+        return Challenge(new AuthenticationProperties { RedirectUri = Url.Action(nameof(LoggingIn)) }, OpenIdConnectDefaults.AuthenticationScheme);
+    }
 
-        public ActionResult Index() => View();
+    [HttpGet]
+    public IActionResult LoggingIn()
+    {
+        var isAuthenticated = User?.Identity?.IsAuthenticated ?? false;
+        var authId = User?.FindFirstValue("name");
+        if (!isAuthenticated || string.IsNullOrWhiteSpace(authId))
+            return Redirect("/");
 
-        public ActionResult Error() => View("Error");
+        var userAccount = MassiveDB.Current.FindUser(authId);
+        if (userAccount == null)
+            userAccount = MassiveDB.Current.CreateUser(authId, HttpContext.Connection.RemoteIpAddress.ToString(), HttpContext.UserDistanceUnits());
 
-        [HttpPost]
-        public IActionResult Login()
+        HttpContext.Session.Set<long?>("rp_UserAccount", (long?)userAccount.Id);
+        HttpContext.Response.Cookies.Append("rp_UserAccount", Secure.EncryptValue(userAccount.Id.ToString()), new CookieOptions { Expires = DateTime.UtcNow.AddYears(1) });
+
+        if (userAccount.UserType == "N")
+            return RedirectToAction("FirstTime", "User");
+
+        var returnPage = HttpContext.Session.GetString("login_returnPage");
+        if (string.IsNullOrWhiteSpace(returnPage))
+            return RedirectToAction("Index", "Home");
+        return Redirect(returnPage);
+    }
+
+    [HttpGet, HttpPost]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return SignOut(CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
+    }
+
+    [HttpPost]
+    public ActionResult UpdateDistanceUnits(int distanceUnit)
+    {
+        logger.LogInformation("Updating units for user to " + distanceUnit);
+        if (!Enum.IsDefined(typeof(DistanceUnits), distanceUnit))
+            return Json(new { Completed = false, Reason = "Invalid distance unit." });
+
+        var newDistanceUnit = (DistanceUnits)distanceUnit;
+        HttpContext.Session.Set("rp_UserDistanceUnits", newDistanceUnit);
+
+        var userAccount = HttpContext.UserAccount();
+        if (userAccount != null)
         {
-            var returnPage = Request.Form["return_page"].FirstOrDefault();
-            if (string.IsNullOrWhiteSpace(returnPage)) returnPage = Url.Content("~/");
-            Uri returnPageUri;
-            Uri.TryCreate(returnPage, UriKind.RelativeOrAbsolute, out returnPageUri);
-            if (returnPageUri == null || returnPageUri.IsAbsoluteUri)
-                returnPage = Url.Content("~/");
-            HttpContext.Session.SetString("login_returnPage", returnPage);
-
-            return Challenge(new AuthenticationProperties { RedirectUri = Url.Action(nameof(LoggingIn)) }, OpenIdConnectDefaults.AuthenticationScheme);
+            userAccount.DistanceUnits = newDistanceUnit;
+            MassiveDB.Current.UpdateUser(userAccount);
         }
 
-        [HttpGet]
-        public IActionResult LoggingIn()
-        {
-            var isAuthenticated = User?.Identity?.IsAuthenticated ?? false;
-            var authId = User?.FindFirstValue("sub");
-            if (!isAuthenticated || string.IsNullOrWhiteSpace(authId))
-                return Redirect("/");
-
-            var userAccount = MassiveDB.Current.FindUser(authId);
-            if (userAccount == null)
-                userAccount = MassiveDB.Current.CreateUser(authId, HttpContext.Connection.RemoteIpAddress.ToString(), HttpContext.UserDistanceUnits());
-
-            HttpContext.Session.Set<long?>("rp_UserAccount", (long?)userAccount.Id);
-            HttpContext.Response.Cookies.Append("rp_UserAccount", Secure.EncryptValue(userAccount.Id.ToString()), new CookieOptions { Expires = DateTime.UtcNow.AddYears(1) });
-
-            if (userAccount.UserType == "N")
-                return RedirectToAction("FirstTime", "User");
-
-            var returnPage = HttpContext.Session.GetString("login_returnPage");
-            if (string.IsNullOrWhiteSpace(returnPage))
-                return RedirectToAction("Index", "Home");
-            return Redirect(returnPage);
-        }
-
-        [HttpGet, HttpPost]
-        public IActionResult Logout()
-        {
-            HttpContext.Session.Clear();
-            return SignOut(CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
-        }
-
-        [HttpPost]
-        public ActionResult UpdateDistanceUnits(int distanceUnit)
-        {
-            logger.LogInformation("Updating units for user to " + distanceUnit);
-            if (!Enum.IsDefined(typeof(DistanceUnits), distanceUnit))
-                return Json(new { Completed = false, Reason = "Invalid distance unit." });
-
-            var newDistanceUnit = (DistanceUnits)distanceUnit;
-            HttpContext.Session.Set("rp_UserDistanceUnits", newDistanceUnit);
-
-            var userAccount = HttpContext.UserAccount();
-            if (userAccount != null)
-            {
-                userAccount.DistanceUnits = newDistanceUnit;
-                MassiveDB.Current.UpdateUser(userAccount);
-            }
-
-            return Json(new { Completed = true });
-        }
+        return Json(new { Completed = true });
     }
 }
--- RunnersPal.Core/Startup.cs
+++ RunnersPal.Core/Startup.cs
@@ -14,109 +14,95 @@ using RunnersPal.Core.Data;
 using RunnersPal.Core.Data.Caching;
 using RunnersPal.Core.ViewModels.Binders;
 
-namespace RunnersPal.Core
+namespace RunnersPal.Core;
+
+public class Startup(IConfiguration configuration)
 {
-    public class Startup
+    public void ConfigureServices(IServiceCollection services)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services
-                .AddAuthentication(o =>
-                {
-                    o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                })
-                .AddCookie(o =>
-                {
-                    o.LoginPath = "/login";
-                    o.LogoutPath = "/logout";
-                    o.Cookie.HttpOnly = true;
-                    o.Cookie.MaxAge = TimeSpan.FromDays(150);
-                    o.ExpireTimeSpan = TimeSpan.FromDays(150);
-                    o.SlidingExpiration = true;
-                })
-                .AddOpenIdConnect(options =>
-                {
-                    options.ClientId = "runnerspal";
-                    options.ClientSecret = "4b301164-0c33-454c-969d-050907f11d55";
-
-                    options.RequireHttpsMetadata = false;
-                    options.GetClaimsFromUserInfoEndpoint = true;
-                    options.SaveTokens = true;
-                    options.ResponseType = OpenIdConnectResponseType.Code;
-                    options.AuthenticationMethod = OpenIdConnectRedirectBehavior.RedirectGet;
-                    options.Authority = "https://smallauth.nosuchblogger.com/";
-                    options.Scope.Add("roles");
-
-                    options.SecurityTokenValidator = new JwtSecurityTokenHandler
-                    {
-                        InboundClaimTypeMap = new Dictionary<string, string>()
-                    };
-
-                    options.TokenValidationParameters.NameClaimType = "name";
-                    options.TokenValidationParameters.RoleClaimType = "role";
-
-                    options.AccessDeniedPath = "/";
-                });
-            
-            services.AddRazorPages();
-            services.AddDistributedMemoryCache();
-            services.AddSession();
-            services
-                .AddMvc(options =>
-                {
-                    options.EnableEndpointRouting = false;
-                    options.ModelBinderProviders.Insert(0, new CustomModelsBinderProvider());
-                })
-                .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
-
-            services.AddLogging(logging =>
+        services
+            .AddAuthentication(o =>
             {
-                logging.AddConsole();
-                logging.AddDebug();
+                o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie(o =>
+            {
+                o.LoginPath = "/login";
+                o.LogoutPath = "/logout";
+                o.Cookie.HttpOnly = true;
+                o.Cookie.MaxAge = TimeSpan.FromDays(150);
+                o.ExpireTimeSpan = TimeSpan.FromDays(150);
+                o.SlidingExpiration = true;
+            })
+            .AddOpenIdConnect(options =>
+            {
+                options.ClientId = "runnerspal";
+                options.ClientSecret = "4b301164-0c33-454c-969d-050907f11d55";
+
+                options.RequireHttpsMetadata = false;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.SaveTokens = true;
+                options.ResponseType = OpenIdConnectResponseType.Code;
+                options.AuthenticationMethod = OpenIdConnectRedirectBehavior.RedirectGet;
+                options.Authority = "https://smallauth.nosuchblogger.com/";
+                options.Scope.Add("roles");
+                options.TokenHandler = new JwtSecurityTokenHandler { InboundClaimTypeMap = new Dictionary<string, string>() };
+                options.UseSecurityTokenValidator = true;
+                options.TokenValidationParameters.NameClaimType = "name";
+                options.TokenValidationParameters.RoleClaimType = "role";
+                options.AccessDeniedPath = "/";
             });
 
-            services.AddTransient<IDataCache, SimpleDataCache>();
-            
-            MassiveDB.Configure(Configuration);
-        }
+        services.AddRazorPages();
+        services.AddDistributedMemoryCache();
+        services.AddSession();
+        services
+            .AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false;
+                options.ModelBinderProviders.Insert(0, new CustomModelsBinderProvider());
+            })
+            .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        services.AddLogging(logging =>
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/home/error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            logging.AddConsole();
+            logging.AddDebug();
+        });
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
+        services.AddTransient<IDataCache, SimpleDataCache>();
 
-            app.UseCookiePolicy();
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.UseSession();
+        MassiveDB.Configure(configuration);
+    }
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapDefaultControllerRoute();
-            });
-
-            app.UseMvcWithDefaultRoute();
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
         }
+        else
+        {
+            app.UseExceptionHandler("/home/error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        app.UseCookiePolicy();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseSession();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapDefaultControllerRoute();
+        });
+
+        app.UseMvcWithDefaultRoute();
     }
 }
