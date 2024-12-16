ccf956b716e7eedf3eae83835a9072a2c3f262ac
Andy Whitfield <andy@andywhitfield.me.uk>
2024-12-16 16:56:02 +0000

    Added auth attribute to user profile page

--- RunnersPal.Core/Pages/User/Profile.cshtml.cs
+++ RunnersPal.Core/Pages/User/Profile.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RunnersPal.Core.Pages.User;
 
+[Authorize]
 public class ProfileModel : PageModel
 {
     public Task OnGet() => Task.CompletedTask;
