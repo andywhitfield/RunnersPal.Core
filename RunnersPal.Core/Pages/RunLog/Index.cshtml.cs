2ce291cf301c639f6c5280163b5d67ecf377205a
Andy Whitfield <andy@andywhitfield.me.uk>
2025-01-21 15:05:14 +0000

    Fixed another todo - show a toast popup if we cannot load run activities

--- RunnersPal.Core/Pages/RunLog/Index.cshtml.cs
+++ RunnersPal.Core/Pages/RunLog/Index.cshtml.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RunnersPal.Core.Services;
 
 namespace RunnersPal.Core.Pages.RunLog;
 
-public class IndexModel : PageModel
+public class IndexModel(IUserService userService) : PageModel
 {
     [BindProperty(SupportsGet = true)] public DateTime Date { get; set; } = DateTime.Today;
     public string InitialDate => Date.ToString("yyy-MM-dd");
 
     public void OnGet() { }
+
+    public bool IsLoggedIn => userService.IsLoggedIn;
 }
